138655f72b543b8cd75ed1daa2af960a80e156f2
Shintaro0221 <sintaronettt@gmail.comt>
2022-06-04 11:35:57 -0700

    fix: organizations module

--- src/modules/organizations.ts
+++ src/modules/organizations.ts
@@ -1,13 +1,12 @@
 import { BigInt, Bytes } from '@graphprotocol/graph-ts'
 import { Organization } from '../../generated/schema'
 export namespace organizations {
-  export function getOrCreateOrganization(organizationAddress: Bytes, timestamp: BigInt): Organization {
+  export function getOrCreateOrganization(organizationAddress: BigInt, timestamp: BigInt): Organization {
     let organizationId = organizationAddress.toHexString()
     let organization = Organization.load(organizationId)
 
     if (organization == null) {
       organization = new Organization(organizationId)
-      organization.name = organizationName
       organization.jobs = new Array<string>()
       organization.createdAt = timestamp
     }
